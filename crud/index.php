10099c3519255e8fa9341439047c372d28141b0a
Lucas Rafael <56982485+lucasrafaeldesouza@users.noreply.github.com>
2022-09-19 10:19:02 -0300

    crud + mysql

--- crud/index.php
+++ crud/index.php
@@ -25,40 +25,40 @@ include_once 'includes/message.php';
                 $sql = "SELECT * FROM clientes";
                 $resultado = mysqli_query($connect, $sql);
 
-                if(mysqli_num_rows($resultado) > 0):
+                if (mysqli_num_rows($resultado) > 0) :
 
 
-                while ($dados = mysqli_fetch_array($resultado)) :
+                    while ($dados = mysqli_fetch_array($resultado)) :
                 ?>
-                    <tr>
-                        <td><?php echo $dados['nome']; ?></td>
-                        <td><?php echo $dados['sobrenome']; ?></td>
-                        <td><?php echo $dados['email']; ?></td>
-                        <td><?php echo $dados['idade']; ?></td>
-                        <td><a href="editar.php?id=<?php echo $dados['id']; ?>" class="btn-floating orange"><i class="material-icons">edit</i></a></td>
-                        <td><a href="#modal <?php echo $dados['id']; ?>" class="btn-floating red modal-trigger"><i class="material-icons">delete</i></a></td>
+                        <tr>
+                            <td><?php echo $dados['nome']; ?></td>
+                            <td><?php echo $dados['sobrenome']; ?></td>
+                            <td><?php echo $dados['email']; ?></td>
+                            <td><?php echo $dados['idade']; ?></td>
+                            <td><a href="editar.php?id=<?php echo $dados['id']; ?>" class="btn-floating orange"><i class="material-icons">edit</i></a></td>
+                            <td><a href="#modal <?php echo $dados['id']; ?>" class="btn-floating red modal-trigger"><i class="material-icons">delete</i></a></td>
 
-                        <!-- Estrutura do Modal -->
-                        <div id="modal <?php echo $dados['id']; ?>" class="modal">
-                            <div class="modal-content">
-                                <h4>Opa!</h4>
-                                <p>Tem certeza que deseja excluir esse cliente?</p>
+                            <!-- Estrutura do Modal -->
+                            <div id="modal <?php echo $dados['id']; ?>" class="modal">
+                                <div class="modal-content">
+                                    <h4>Opa!</h4>
+                                    <p>Tem certeza que deseja excluir esse cliente?</p>
+                                </div>
+                                <div class="modal-footer">
+
+
+                                    <form action="php_action/delete.php" method="POST">
+                                        <input type="hidden" name="id" value="<?php echo $dados['id']; ?>">
+                                        <button type="submit" name="btn-deletar" class="btn red">Sim, quero deletar</button>
+                                        <a href="#!" class="modal-close waves-effect waves-green btn-flat">Cancelar</a>
+                                    </form>
+
+                                </div>
                             </div>
-                            <div class="modal-footer">
 
-
-                            <form action="php_action/delete.php" method="POST">
-                                <input type="hidden" name="id" value="<?php echo $dados['id']; ?>">
-                                <button type="submit" name="btn-deletar" class="btn red">Sim, quero deletar</button>
-                                <a href="#!" class="modal-close waves-effect waves-green btn-flat">Cancelar</a>
-                            </form>
-
-                            </div>
-                        </div>
-
-                    </tr>
-                <?php endwhile;
-                else: ?>
+                        </tr>
+                    <?php endwhile;
+                else : ?>
                     <tr>
                         <td>-</td>
                         <td>-</td>
@@ -66,7 +66,7 @@ include_once 'includes/message.php';
                         <td>-</td>
                     </tr>
                 <?php
-                endif; 
+                endif;
                 ?>
             </tbody>
         </table>
